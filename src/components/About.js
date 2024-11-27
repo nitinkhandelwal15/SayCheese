44f0e9dd15d917780c3d4808bc0ef0d5ce82886f
Nitin Khandelwal <khandelwal.nitin.1505@gmail.com>
2024-11-27 16:34:19 +0530

    browserRouter added

---
+++ src/components/About.js
@@ -0,0 +1,10 @@
+const About = () => {
+  return (
+    <div>
+      <h1>This is About us page</h1>
+      <h2>its wonderful that you wanna know about us</h2>
+    </div>
+  );
+};
+
+export default About;
---
+++ src/components/Contact.js
@@ -0,0 +1,10 @@
+const Contact = () => {
+  return (
+    <div>
+      <h1>This is Contact us page</h1>
+      <h2>were you really searching for a contact no.? XD</h2>
+    </div>
+  );
+};
+
+export default Contact;
