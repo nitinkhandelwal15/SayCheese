fe092d9d39d656eccffb54a8aa28230887f183dd
Nitin Khandelwal <khandelwal.nitin.1505@gmail.com>
2025-02-06 15:56:17 +0530

    modularised fetch menu hook

--- src/components/RestaurantMenu.js
+++ src/components/RestaurantMenu.js
@@ -2,26 +2,29 @@ import { useState, useEffect } from "react";
 import Shimmer from "./Shimmer";
 import { useParams } from "react-router-dom";
 import { MENU_API } from "../utils/constants";
+import useRestaurantMenu from "../utils/useRestaurantMenu";
 
 const RestaurantMenu = () => {
-  const [resInfo, setResInfo] = useState(null);
+  //  const [resInfo, setResInfo] = useState(null);
   const { resId } = useParams();
 
   console.log(resId);
 
-  useEffect(() => {
-    fetchMenu();
-  }, []);
+  const resInfo = useRestaurantMenu(resId);
 
-  const fetchMenu = async () => {
-    const data = await fetch(MENU_API + resId); //65797, 393840
-    const json = await data.json();
+  // useEffect(() => {
+  //   fetchMenu();
+  // }, []);
 
-    console.log(json);
-    setResInfo(json.data);
-  };
+  // const fetchMenu = async () => {
+  //   const data = await fetch(MENU_API + resId); //65797, 393840
+  //   const json = await data.json();
 
-  console.log(resInfo);
+  //   console.log(json);
+  //   setResInfo(json.data);
+  // };
+
+  // console.log(resInfo);
 
   if (resInfo === null) {
     return <Shimmer />;
